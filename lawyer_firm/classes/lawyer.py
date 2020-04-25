4514b6c9c81cd166a1a83f9441efb0c4b23e2ef3
aekrops <svyatoslavtrostynski24@gmail.com>
2020-04-25 22:53:37 +0300

    Edited __str__ method in Lawyer, added field services

--- lawyer_firm/classes/lawyer.py
+++ lawyer_firm/classes/lawyer.py
@@ -3,11 +3,13 @@ from lawyer_firm.classes.service import Service
 
 class Lawyer(Service):
 
-    def __init__(self, name=None, age=None, price_in_uah=None, representation_in_court=None, advice=None, collecting_evidence=None):
+    def __init__(self, name=None, age=None, price_in_uah=None, representation_in_court=None, advice=None,
+                 collecting_evidence=None):
         super().__init__(representation_in_court, advice, collecting_evidence)
         self.name = name
         self.age = age
         self.price_in_uah = price_in_uah
+        self.services = Service.create_list_of_services(representation_in_court, advice, collecting_evidence)
 
     def __del__(self):
         return
@@ -16,4 +18,7 @@ class Lawyer(Service):
         name = f'Name: {self.name}\n'
         age = f'Age: {self.age}\n'
         price_in_uah = f'Price: {self.price_in_uah}\n'
-        return name + age + price_in_uah + super().__str__()
+        services = 'Available services: {0}\n'.format(', '.join(self.services))
+        return name + age + price_in_uah + services
+
+
