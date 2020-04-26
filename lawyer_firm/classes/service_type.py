f985d8b5cd56278a031af9ca4de2fc07850eeee0
aekrops <svyatoslavtrostynski24@gmail.com>
2020-04-26 16:51:10 +0300

    deleted unneccesary files

--- lawyer_firm/classes/service_type.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class ServiceEnum(Enum):
-    ADVICE = 'advice'
-    COLLECTINGEVIDENCE = 'collecting_evidence'
-    DRAFTINGACLAIM = 'drafting_a_claim'
-    SIGNINGACONTRACT = 'signing_a_contract'
-    REPRESENTATIONINCOURT = 'representation_in_court'
